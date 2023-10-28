8d8e5232a50db1faabafee13db657e528244cf1f
Sirawitch Tiyasuttipun <57455984+sirawitch@users.noreply.github.com>
2023-10-28 18:05:14 +0700

    Cars24 Web Scrap

---
+++ WebScrappingCode/Cars24_4door/GetData.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import date
+chrome_service = ChromeService(
+    '/Users/sirawitchtiyasuttipun/Downloads/chromedriver-mac-arm64_2/chromedriver')
+choose = 0
+df=pd.DataFrame()
+f = open("cars24_4door_link.txt", "r")
+driver = webdriver.Chrome(service=chrome_service)
+today = date.today()
+id=0
+for line in f:
+        id+=1
+        webid=line
+        driver = webdriver.Chrome(service=chrome_service)
+        driver.get(line)
+        html_data = driver.page_source
+        soup = BeautifulSoup(html_data, "html.parser")
+        name = soup.select('h1._3iDrO')[0].text.strip()
+        addName = soup.select('p.qKPAR')[0].text.split('|')[0].strip()
+        price =soup.select('strong.GBIX0')[0].text.split('บาท')
+        notSalePrice = price[0].strip()
+        salePrice = price[1].strip()
+        info = soup.find("div",class_="_2X4e9").find_all("div",class_="_2X_Gu")
+        for div in info:
+            text = div.get_text()
+            if "ออกรถ" in text:
+                year=text[10:]
+                #print(text[10:])
+            elif "ทะเบียน" in text:
+                tabian=text[8:]
+                #print(text[8:])
+            elif "สี" in text:
+                color=text[2:]
+                #print(text[2:])
+            elif "เกียร์" in text:
+                 gear=text[10:]
+                #print(text[10:])
+            elif "เครื่องยนต์" in text:
+                fuel=text[11:]
+                #print(text[11:])
+            elif "ระยะทาง" in text:
+                km=text[13:-4]
+                #print(text[13:-4])
+        injure = soup.find("div",class_="_1Wk7G").get_text().strip()
+        d=pd.DataFrame({'id':[id],'year':[year],'tabian':[tabian],'name':[name],'addName':[addName],'salePrice':[salePrice],'notSalePrice':[notSalePrice],'distance':[km],'fuel':[fuel],'gear':[gear],'color':[color],'injure':[injure],'door':[4],'address':['Bangkok'],'web':['cars24'],'webid':[webid],'date':[today]})
+        df=pd.concat([df,d])
+        driver.quit()
+        continue
+        if skip==1:
+            print(line)
+            driver.close()
+            continue
+        row =  pd.DataFrame({'Version':[head],'generalInfo':[generalInfo],'distance':[km],'update':[update],'cost':[cost],'other':[otherdata]})
+        df=pd.concat([df,row])
+        driver.close()
+df.to_csv("DataFromCars24.csv",index=False)
+print("Done")
---
+++ WebScrappingCode/Cars24_4door/GetLink.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+chrome_service = ChromeService(
+    '/Users/sirawitchtiyasuttipun/Downloads/chromedriver-mac-arm64_2/chromedriver')
+choose = 0
+driver = webdriver.Chrome(service=chrome_service)
+page=1
+f = open("cars24_4door_link.txt", "w+")
+for year in range(2014,2022):
+    driver.get('https://www.cars24.co.th/buy-used-cars-bangkok/?rf=year:'+str(year)+";"+str(year)+'&sf=make:Mazda&sf=doors:4&sort=relevance&sf=city:TH_BANGKOK')
+    # element=driver.find_element(By.XPATH,"//div[@id='Car_List']")
+    # data= element.get_attribute("innerHTML")
+    html_data = driver.page_source
+    soup = BeautifulSoup(html_data, "html.parser")
+    links = soup.find_all("a")
+    for link in links:
+        if link.get("href") == None:
+            continue
+        elif "buy-used-mazda" in link.get("href") and "THA" in  link.get("href"):
+            f.write(link.get("href")+"\n")
+driver.quit()
+print("Done")
+# with open('WebScrap/data2023.html', "w", encoding="utf-8") as file:
+#    file.write(data)
