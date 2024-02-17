7483688e5f7743e2bb8e11f398f726b262d6382b
ANAN DEESAKUL <tiwyy13@gmail.com>
2024-02-17 16:13:16 +0700

    Delete terraform

--- Airflow/dags/scrape_one2car.py
+++ Airflow/dags/scrape_one2car.py
@@ -3,6 +3,8 @@ import logging
 import pendulum
 from airflow.decorators import task
 from airflow import DAG
+from WebScrappingCode.GetLink import getLink, create_link_object
+from WebScrappingCode.GetData import getData, create_data_object
 
 DAG_NAME = "scrap_one2car"
 log = logging.getLogger(__name__)
@@ -19,25 +21,12 @@ with DAG(
         task_id="get_link_task",
     )
     def get_link_one2car():
-        """
-        Scrape the one2car website to get link of cars to be sold
-        """
-        with open(
-            "/opt/airflow/WebScrappingCode/One2Car/GetLink.py"
-        ) as get_link_one2car_script:
-            exec(get_link_one2car_script.read())
+        getLink()
 
     @task(
         task_id="get_data_task",
     )
     def get_data_one2car():
-        """
-        Scrape the one2car website to get data of links
-        """
-        with open(
-            "/opt/airflow/WebScrappingCode/One2Car/GetData.py"
-        ) as get_data_one2car_script:
-            exec(get_data_one2car_script.read())
+        getData()
 
-    def 
-    get_link_one2car() >> get_data_one2car()
+    get_link_one2car() >> create_link_object >> get_data_one2car() >> create_data_object
--- Airflow/start.sh
+++
@@ -1,3 +0,0 @@
-# mkdir -p ./dags ./logs ./plugins ./config
-docker build -t webscrap-airflow:0.01 -f ./Airflow/Dockerfile .
-docker compose -f ./Airflow/docker-compose.yml up
