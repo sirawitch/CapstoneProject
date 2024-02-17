6360d2ba4e2027f6c4756b0eca124002b31f254a
ANAN DEESAKUL <tiwyy13@gmail.com>
2024-02-17 16:19:44 +0700

    Remove terraform module

--- WebScrappingCode/One2Car/GetData.py
+++ WebScrappingCode/One2Car/GetData.py
@@ -1,112 +1,91 @@
-from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import date, timedelta
-import requests
-import os.path
+def getData():
+    from bs4 import BeautifulSoup
+    import pandas as pd
+    from datetime import date, timedelta
+    import requests
+    import os
 
-today = date.today()
+    today = date.today()
 
-# define path
-link_path = f"/opt/airflow/WebScrapData/One2Car/Link_{today.month}_{today.year}.txt"
-existing_dimension_path = (
-    f"/opt/airflow/WebScrapData/One2Car/dimension_{today.month}_{today.year}.csv"
+    # define path
+    link_path = f"{os.environ['AIRFLOW_HOME']}/link.txt"
+
+    every_car_all_info = pd.DataFrame()
+    link_file = open(link_path, "r")
+
+    for line in link_file[1:3]:
+        webid = line[33:]
+
+        headers = {
+            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+        }
+        respond = requests.get(line, headers=headers)
+        html_data = respond.content
+        print(html_data)
+        soup = BeautifulSoup(html_data, "html.parser")
+        costDiv = soup.find(
+            "div",
+            class_="u-order-1@mobile u-margin-right-xs@mobile u-color-white u-text-3 u-text-4@mobile u-text-bold u-margin-bottom-none u-margin-top-xxs",
+        )
+
+        # Extract and print the text from each div
+        if costDiv == None:
+            continue
+        skip = 0
+        afterbaht = 0
+        otherdata = ""
+        text_data = costDiv.get_text()
+        if "บาท" in text_data:
+            cost = text_data[0:-4]
+        name = soup.select("h1.listing__title")[0].text.strip()
+        div = soup.find_all(
+            "div", class_="c-card__body u-bg-haze-light u-flex u-flex--column"
+        )
+        for i in div:
+            text = i.get_text()
+            if "สภาพ" in text:
+                status = text[9:-1]
+            elif "เลขไมล์" in text:
+                mile = text[18:-1]
+            elif "สี" in text:
+                color = text[7:-1]
+            elif "ระบบเกียร์" in text:
+                gear = text[15:-1]
+        car_all_info = pd.DataFrame(
+            {
+                "name": [name],
+                "cost": [cost],
+                "mile": [mile],
+                "status": [status],
+                "color": [color],
+                "gear": [gear],
+                "web": ["one2car"],
+                "webid": [webid],
+                "date": [today],
+            }
+        )
+
+        every_car_all_info = pd.concat([every_car_all_info, car_all_info])
+
+    every_car_all_info.to_csv(
+        f"{os.environ['AIRFLOW_HOME']}data.csv",
+        index=False,
+    )
+    link_file.close()
+    print("Done")
+
+
+from datetime import date
+from airflow.providers.amazon.aws.transfers.local_to_s3 import (
+    LocalFilesystemToS3Operator,
 )
+import os
 
-# init var
-every_car_basic_info = pd.DataFrame()
-every_car_transaction_info = pd.DataFrame()
-every_car_all_info = pd.DataFrame()
-link_file = open(link_path, "r")
-dimension_column_names = ["webid", "name", "mile", "color", "gear", "web"]
-transaction_column_names = ["webid", "status", "cost", "date"]
-existing_dimension_df = None
-
-# check for existing car information file
-if os.path.isfile(existing_dimension_path):
-    existing_dimension_df = pd.read_csv(existing_dimension_path)
-else:
-    existing_dimension_df = pd.DataFrame(columns=dimension_column_names)
-
-for line in link_file:
-    webid = line[33:]
-
-    headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
-    }
-    respond = requests.get(line, headers=headers)
-    html_data = respond.content
-
-    soup = BeautifulSoup(html_data, "html.parser")
-    costDiv = soup.find(
-        "div",
-        class_="u-order-1@mobile u-margin-right-xs@mobile u-color-white u-text-3 u-text-4@mobile u-text-bold u-margin-bottom-none u-margin-top-xxs",
-    )
-
-    # Extract and print the text from each div
-    if costDiv == None:
-        continue
-    skip = 0
-    afterbaht = 0
-    otherdata = ""
-    text_data = costDiv.get_text()
-    if "บาท" in text_data:
-        cost = text_data[0:-4]
-    name = soup.select("h1.listing__title")[0].text.strip()
-    div = soup.find_all(
-        "div", class_="c-card__body u-bg-haze-light u-flex u-flex--column"
-    )
-    for i in div:
-        text = i.get_text()
-        if "สภาพ" in text:
-            status = text[9:-1]
-        elif "เลขไมล์" in text:
-            mile = text[18:-1]
-        elif "สี" in text:
-            color = text[7:-1]
-        elif "ระบบเกียร์" in text:
-            gear = text[15:-1]
-    car_all_info = pd.DataFrame(
-        {
-            "name": [name],
-            "cost": [cost],
-            "mile": [mile],
-            "status": [status],
-            "color": [color],
-            "gear": [gear],
-            "web": ["one2car"],
-            "webid": [webid],
-            "date": [today],
-        }
-    )
-    # webid as identifier of a car
-
-    dim_car_attribute = pd.DataFrame(
-        {
-            "webid": [webid],
-            "name": [name],
-            "mile": [mile],
-            "color": [color],
-            "gear": [gear],
-            "web": ["one2car"],
-        }
-    )
-
-    fact_car_transcation = pd.DataFrame(
-        {
-            "webid": [webid],
-            "status": [status],
-            "cost": [cost],
-            "date": [today],
-        }
-    )
-
-    every_car_all_info = pd.concat([every_car_all_info, car_all_info])
-
-every_car_all_info.to_csv(
-    f"/opt/airflow/WebScrapData/One2Car/{today.month}_{today.year}.csv",
-    index=False,
-    mode="a",
+create_data_object = LocalFilesystemToS3Operator(
+    task_id="create_data_object",
+    dest_bucket="aws-ia-mwaa-502983918849",
+    dest_key=f"data/data-{date.today()}.csv",
+    filename=f"{os.environ['AIRFLOW_HOME']}/data.csv",
+    replace=True,
+    aws_conn_id="s3_conn",
 )
-
-
-print("Done")
--- WebScrappingCode/One2Car/GetLink.py
+++ WebScrappingCode/One2Car/GetLink.py
@@ -1,31 +1,47 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import requests
+def getLink():
 
-f = open("/opt/airflow/WebScrapData/One2Car/Link_new.txt", "w+")
+    from bs4 import BeautifulSoup
+    import requests
+    import os
 
-for page in range(1, 97):
-    headers = {
-        "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
-    }
-    one2car_URL = "https://www.one2car.com/รถ-สำหรับ-ขาย/mazda?type=used&page_size=26&page_number="
-    respond = requests.get(one2car_URL + f"{page}", headers=headers)
-    html_data = respond.content
+    f = open(f"{os.environ['AIRFLOW_HOME']}/link.txt", "w")
 
-    soup = BeautifulSoup(html_data, "html.parser")
-    links = soup.find_all("a")
-    laststr = "test"
-    for link in links:
-        if link.get("href") == None:
-            continue
-        if "one2car.com/for-sale" in link.get("href"):
-            if laststr == link.get("href"):
+    # for page in range(1, 97):
+    for page in range(1, 2):
+        headers = {
+            "User-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+        }
+        one2car_URL = "https://www.one2car.com/รถ-สำหรับ-ขาย/mazda?type=used&page_size=26&page_number="
+        respond = requests.get(one2car_URL + f"{page}", headers=headers)
+        html_data = respond.content
+
+        soup = BeautifulSoup(html_data, "html.parser")
+        links = soup.find_all("a")
+        laststr = "test"
+        for link in links:
+            if link.get("href") == None:
                 continue
-            laststr = link.get("href")
-            f.write(link.get("href") + "\n")
+            if "one2car.com/for-sale" in link.get("href"):
+                if laststr == link.get("href"):
+                    continue
+                laststr = link.get("href")
+                f.write(link.get("href") + "\n")
 
-f.close()
-print("Done")
+    f.close()
+    print("Done")
+
+
+from datetime import date
+from airflow.providers.amazon.aws.transfers.local_to_s3 import (
+    LocalFilesystemToS3Operator,
+)
+import os
+
+create_link_object = LocalFilesystemToS3Operator(
+    task_id="create_link_object",
+    dest_bucket="aws-ia-mwaa-502983918849",
+    dest_key=f"data/link-{date.today()}.txt",
+    filename=f"{os.environ['AIRFLOW_HOME']}/link.txt",
+    replace=True,
+    aws_conn_id="s3_conn",
+)
